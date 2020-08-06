d03160e9bf5a090c96de69c857dc01040140a300
filipek <software@pek.com>
2020-08-06 11:12:26 +0200

    Add boostrap-docker-ssl for use in UserData sections of YAML templates. Rename file.

---
+++ generate-certificates.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+while getopts "s:p:u:" OPTION; do
+    case ${OPTION} in
+    s)
+        STACK_NAME=${OPTARG}
+        if [ -z ${STACK_NAME} ]; then
+            echo "Stack name is required, use -s xyz"
+            exit 1
+        fi
+        ;;
+    p)
+        PRIVATE_IP=${OPTARG}
+        if [ -z "${PRIVATE_IP}" ]; then
+            echo "Private IP address is required, use -p xyz"
+            exit 1
+        fi
+        ;;
+    u)
+        PUBLIC_IP=${OPTARG}
+        if [ -z "${PUBLIC_IP}" ]; then
+            echo "Public IP address is required, use -u xyz"
+            exit 1
+        fi
+        ;;
+    *)
+        echo "Incorrect options provided"
+        exit 1
+        ;;
+    esac
+done
+
+if [ -z "${STACK_NAME}" ]; then
+    echo "Stack name is required, use -s xyz"
+    exit 1
+fi
+if [ -z "${PRIVATE_IP}" ]; then
+    echo "Private IP address is required, use -p xyz"
+    exit 1
+fi
+if [ -z "${PUBLIC_IP}" ]; then
+    echo "Public IP address is required, use -u xyz"
+    exit 1
+fi
+
+GENPKEY_OPTS="-algorithm RSA -pkeyopt rsa_keygen_bits:2048"
+TEN_YEARS_IN_DAYS=3650
+
+TS=$(date +%Y%m%d_%H%M%S)
+#OUT_DIR=docker-tls-${TS}
+OUT_DIR=${STACK_NAME}-${TS}
+DOCKER_DIR=/etc/docker/ssl
+
+mkdir ${OUT_DIR}
+chmod 0700 ${OUT_DIR}/
+cd ${OUT_DIR}
+
+###########################################################
+# CA key
+openssl genpkey ${GENPKEY_OPTS} -out ca-key.pem
+
+# CA certificate
+openssl req -new -x509 -days ${TEN_YEARS_IN_DAYS} -subj "/CN=${STACK_NAME}-ca"  -key ca-key.pem -sha256 -out ca.pem
+
+###########################################################
+# Server key
+openssl genpkey ${GENPKEY_OPTS} -out server-key.pem
+
+# Server CSR
+openssl req -new -subj "/CN=${STACK_NAME}-server" -key server-key.pem -out server.csr
+
+# Alts IPs
+echo "subjectAltName = IP:${PUBLIC_IP},IP:${PRIVATE_IP},IP:127.0.0.1" > server-extfile.cnf
+# Server certificate
+openssl x509 -req -days ${TEN_YEARS_IN_DAYS} \
+    -extfile server-extfile.cnf -in server.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem
+
+###########################################################
+# Client key
+openssl genpkey ${GENPKEY_OPTS} -out client-key.pem
+
+# Client CSR
+openssl req -new -subj "/CN=${STACK_NAME}-client" -key client-key.pem -out client.csr
+
+# clientAuth
+echo "extendedKeyUsage = clientAuth" > client-extfile.cnf
+# Client certificate
+openssl x509 -req -days ${TEN_YEARS_IN_DAYS} \
+    -extfile client-extfile.cnf -in client.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem
+
+# Securing
+chmod -v 0444 *
+chmod -v 0400 *-key.pem
+
+cd ..
+
+# Moving
+sudo mkdir -p ${DOCKER_DIR}
+sudo chown root:docker ${DOCKER_DIR}
+sudo chmod 700 ${DOCKER_DIR}
+sudo cp ${OUT_DIR}/{ca,server-*}.pem ${DOCKER_DIR}
+
+exit 0;
---
+++ ssl/bootstrap-docker-ssl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+while getopts "s:" OPTION; do
+    case ${OPTION} in
+    s)
+        STACK_NAME=${OPTARG}
+        if [ -z ${STACK_NAME} ]; then
+            echo "Stack name is required, use -s xyz"
+            exit 1
+        fi
+        ;;
+    *)
+        echo "Incorrect options provided"
+        exit 1
+        ;;
+    esac
+done
+
+if [ -z "${STACK_NAME}" ]; then
+    echo "Stack name is required, use -s xyz"
+    exit 1
+fi
+
+PUBLIC_IP=$(curl http://checkip.amazonaws.com/)
+PRIVATE_IP=$(hostname | cut -d- -f2- | sed 's/-/./g')
+SCRIPT_ROOT=https://raw.githubusercontent.com/software-pek-com/scripts/master/ssl
+
+# Assumes we are in a directory where we can work freely e.g. /tmp.
+
+curl ${SCRIPT_ROOT}/generate-certificates.sh >> generate-certificates.sh
+curl ${SCRIPT_ROOT}/setup-docker-certificates.sh >> setup-docker-certificates.sh
+chmod 755 generate-certificates.sh setup-docker-certificates.sh
+
+./generate-certificates.sh -s ${STACK_NAME} -p ${PRIVATE_IP} -u ${PUBLIC_IP}
+./setup-docker-certificates.sh
+
+exit 0;
