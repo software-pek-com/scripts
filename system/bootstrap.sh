c1a8be25d6cfbb66f064f655d100358ff78e65af
filipek <software@pek.com>
2020-08-10 00:48:10 +0200

    Refactor and clean up.

--- system/bootstrap.sh
+++ system/bootstrap.sh
@@ -5,10 +5,10 @@
 function print_usage {    
     local me=`basename "$0"`
     echo "usage: $me [-d domainname] [-n stackname] [-s snapshotid] [-v]"
-    echo "  -d domainname e.g. -d xyz.com"
-    echo "  -n stackname e.g. -n xyz-com"
-    echo "  -s snapshotid e.g. -s snap-123"
-    echo "  -v create volume directories e.g. -v"
+    echo "  -d domainname (required) e.g. -d xyz.com"
+    echo "  -n stackname (required) e.g. -n xyz-com"
+    echo "  -s snapshotid (optional) e.g. -s snap-123"
+    echo "  -v create volume directories (optional) e.g. -v"
 }
 
 # We need 3 options with values so there must be 6 script arguments
@@ -55,6 +55,32 @@ SCRIPT_ROOT=https://raw.githubusercontent.com/software-pek-com/scripts/master
 #####################################################################
 # Functions
 
+function mount_snapshot {
+    if ! [ -z ${SNAPSHOT_ID} ]; then
+        mkdir /mnt/${SNAPSHOT_ID}
+        mkfs.xfs /dev/xvdf
+        echo "/dev/xvdf	/mnt/${SNAPSHOT_ID}	auto	defaults,nofail	0	2" >> /etc/fstab
+        mount -a
+        ln -s /mnt/${SNAPSHOT_ID} /mnt/${STACK_NAME}
+    fi
+}
+
+function update_and_install {
+    apt-get -y update
+    apt-get -y install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        docker-compose \
+        gnupg-agent \
+        software-properties-common
+        
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt-get -y install docker-ce docker-ce-cli containerd.io
+    usermod -aG docker ubuntu
+}
+
 function bootstrap_docker_tls {
     curl https://raw.githubusercontent.com/software-pek-com/scripts/master/docker/bootstrap-docker-tls.sh > /tmp/bootstrap-docker-tls.sh
     chmod 755 /tmp/bootstrap-docker-tls.sh
@@ -63,10 +89,12 @@ function bootstrap_docker_tls {
 }
 
 function create_volume_directories {
-    curl ${SCRIPT_ROOT}/docker/create-volume-directories.sh > create-volume-directories.sh
+    if ! [ -z ${CREATE_VOLUME_DIRECTORIES+x} ]; then
+        curl ${SCRIPT_ROOT}/docker/create-volume-directories.sh > create-volume-directories.sh
 
-    chmod 755 create-volume-directories.sh
-    ./create-volume-directories.sh -d ${DOMAIN_NAME} -p /mnt/${STACK_NAME}
+        chmod 755 create-volume-directories.sh
+        ./create-volume-directories.sh -d ${DOMAIN_NAME} -p /mnt/${STACK_NAME}
+    fi
 }
 
 function create_bootstrap_log {
@@ -80,30 +108,10 @@ function create_bootstrap_log {
 # Body
 
 create_bootstrap_log
-# exit 0;
-
-mkdir /mnt/${SNAPSHOT_ID}
-mkfs.xfs /dev/xvdf
-echo "/dev/xvdf	/mnt/${SNAPSHOT_ID}	auto	defaults,nofail	0	2" >> /etc/fstab
-mount -a
-ln -s /mnt/${SNAPSHOT_ID} /mnt/${STACK_NAME}
-
-apt-get -y update
-apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    docker-compose \
-    gnupg-agent \
-    software-properties-common
-    
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get -y install docker-ce docker-ce-cli containerd.io
-usermod -aG docker ubuntu
-
-if [ -z ${CREATE_VOLUME_DIRECTORIES+x} ]; then
-    create_volume_directories
-fi
-
 exit 0;
+
+# mount_snapshot
+# update_and_install
+# create_volume_directories
+
+# exit 0;
