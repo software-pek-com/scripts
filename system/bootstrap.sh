1e7e401b08f849a864ef18068f8a146b37149054
filipek <software@pek.com>
2020-08-10 01:15:52 +0200

    Change cmd arg parsing to allow optional args easily. Other refactoring.

--- system/bootstrap.sh
+++ system/bootstrap.sh
@@ -4,59 +4,90 @@
 # Usage
 function print_usage {    
     local me=`basename "$0"`
-    echo "usage: $me [-d domainname] [-n stackname] [-s snapshotid] [-v]"
+
+    if ! [ -z ${1+x} ]; then # If it is set.
+        echo "Missing required option $1"
+    fi
+
+    echo "Usage: $me [-d domainname] [-n stackname] ?[-s snapshotid] ?[-v]"
     echo "  -d domainname (required) e.g. -d xyz.com"
     echo "  -n stackname (required) e.g. -n xyz-com"
     echo "  -s snapshotid (optional) e.g. -s snap-123"
     echo "  -v create volume directories (optional) e.g. -v"
 }
 
-# We need 3 options with values so there must be 6 script arguments
-if [ $# -ne 6 ]; then
+POSITIONAL=()
+while [ $# -gt 0 ];
+do
+key="$1"
+
+case $key in
+    -d|--domainname)
+    DOMAIN_NAME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -n|--stackname)
+    STACK_NAME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -s|--snapshotid)
+    SNAPSHOT_ID="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -v|--volumedirs)
+    CREATE_VOLUME_DIRECTORIES="1" # This is a flag.
+    shift # past argument
+    shift # past value
+    ;;
+    --help)
+    shift # past argument
     print_usage
+    exit 0
+    ;;
+    *)    # unknown option
+    #POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    echo "Unknown option $key"
+    print_usage
+    exit 0
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# Check required options are set
+
+if [ -z ${DOMAIN_NAME} ]; then # If it is unset.
+    print_usage "-d"
     exit 1
 fi
 
-while getopts "d:n:s:v" OPTION; do
-    case ${OPTION} in
-    d)
-        DOMAIN_NAME=${OPTARG}
-        if [ -z ${DOMAIN_NAME} ]; then
-            print_usage
-            exit 1
-        fi
-        ;;
-    n)
-        STACK_NAME=${OPTARG}
-        if [ -z ${STACK_NAME} ]; then
-            print_usage
-            exit 1
-        fi
-        ;;
-    s)
-        SNAPSHOT_ID=${OPTARG}
-        if [ -z ${SNAPSHOT_ID} ]; then
-            print_usage
-            exit 1
-        fi
-        ;;
-    v)
-        CREATE_VOLUME_DIRECTORIES=1
-        ;;
-    *)
-        print_usage
-        exit 1
-        ;;
-    esac
-done
+if [ -z ${STACK_NAME} ]; then # If it is unset.
+    print_usage "-n"
+    exit 1
+fi
 
 SCRIPT_ROOT=https://raw.githubusercontent.com/software-pek-com/scripts/master
 
 #####################################################################
 # Functions
 
+function create_bootstrap_log {
+    echo "DomainName: ${DOMAIN_NAME}" >> boostrap.log
+    echo "StackName: ${STACK_NAME}" >> boostrap.log
+    echo "SnapshotId: ${SNAPSHOT_ID}" >> boostrap.log
+    echo "VolumeDirs: ${CREATE_VOLUME_DIRECTORIES}" >> boostrap.log
+    # echo "DomainName: ${DOMAIN_NAME}"
+    # echo "StackName: ${STACK_NAME}"
+    # echo "SnapshotId: ${SNAPSHOT_ID}"
+    # echo "VolumeDirs: '${CREATE_VOLUME_DIRECTORIES}'"
+}
+
 function mount_snapshot {
-    if ! [ -z ${SNAPSHOT_ID} ]; then
+    if ! [ -z ${SNAPSHOT_ID} ]; then # If it is set.
         mkdir /mnt/${SNAPSHOT_ID}
         mkfs.xfs /dev/xvdf
         echo "/dev/xvdf	/mnt/${SNAPSHOT_ID}	auto	defaults,nofail	0	2" >> /etc/fstab
@@ -89,7 +120,7 @@ function bootstrap_docker_tls {
 }
 
 function create_volume_directories {
-    if ! [ -z ${CREATE_VOLUME_DIRECTORIES+x} ]; then
+    if ! [ -z ${CREATE_VOLUME_DIRECTORIES+x} ]; then # If it is set.
         curl ${SCRIPT_ROOT}/docker/create-volume-directories.sh > create-volume-directories.sh
 
         chmod 755 create-volume-directories.sh
@@ -97,21 +128,14 @@ function create_volume_directories {
     fi
 }
 
-function create_bootstrap_log {
-    echo "DomainName: ${DOMAIN_NAME}" >> boostrap.log
-    echo "StackName: ${STACK_NAME}" >> boostrap.log
-    echo "Snapshot: ${SNAPSHOT_ID}" >> boostrap.log
-    echo "ScriptRoot: ${SCRIPT_ROOT}" >> boostrap.log
-}
-
 #####################################################################
 # Body
 
 create_bootstrap_log
-exit 0;
+# exit 0;
 
-# mount_snapshot
-# update_and_install
-# create_volume_directories
+mount_snapshot
+update_and_install
+create_volume_directories
 
 # exit 0;
